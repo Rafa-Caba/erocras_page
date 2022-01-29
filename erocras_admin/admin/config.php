5a16e390649b3e3200978574eb0fff3609a05dac
Rafael <rafaelcabanillas12@gmail.com>
2022-01-29 10:53:59 -0600

    Third commit

---
+++ erocras_admin/admin/config.php
@@ -0,0 +1,25 @@
+<?php
+
+// define('RUTA_ERO', 'http://localhost/erocras-page/');
+define('RUTA_ERO', 'https://erocras-page.herokuapp.com/');
+//new mysqli('sql303.epizy.com', 'epiz_30791266', 'IW4ZLhnRZy', 'epiz_30791266_invitados_boda');
+
+/*
+$bd_config = array(
+    'host'      => 'localhost',
+    'basedatos' => 'cantos_erocras',
+    'usuario'   => 'root',
+    'pass'      => ''
+);
+*/
+
+$bd_config = array(
+    'host'      => 'us-cdbr-east-05.cleardb.net',
+    'basedatos' => 'heroku_e49ffd4631c0a87',
+    'usuario'   => 'bc471f03c52ff5',
+    'pass'      => '1a07780a'
+);
+
+define('CARPETA_IMAGES', 'images/');
+
+define('CODIGO_LOGIN', 3162014);
